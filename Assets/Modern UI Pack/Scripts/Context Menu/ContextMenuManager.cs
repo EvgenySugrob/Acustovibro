9ad72f1805bc004fc66d62a54a5972f1145d79ca
EvgenySugrob <xgenstudioz0@gmail.com>
2023-11-11 15:44:40 +0500

    Assets(Folder models-postProcess)

---
+++ Assets/Modern UI Pack/Scripts/Context Menu/ContextMenuManager.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace Michsky.MUIP
+{
+    [RequireComponent(typeof(Animator))]
+    public class ContextMenuManager : MonoBehaviour, IPointerExitHandler
+    {
+        // Resources
+        public Canvas mainCanvas;
+        public Camera targetCamera;
+        public GameObject contextContent;
+        public GameObject trigger;
+        public GameObject shadow;
+        public Animator contextAnimator;
+        public GameObject contextButton;
+        public GameObject contextSeparator;
+        public GameObject contextSubMenu;
+
+        // Settings
+        public bool autoSubMenuPosition = true;
+        public SubMenuBehaviour subMenuBehaviour;
+        public CameraSource cameraSource = CameraSource.Main;
+
+        // Bounds
+        [Range(-50, 50)] public int vBorderTop = -10;
+        [Range(-50, 50)] public int vBorderBottom = 10;
+        [Range(-50, 50)] public int hBorderLeft = 15;
+        [Range(-50, 50)] public int hBorderRight = -15;
+
+        Vector2 uiPos;
+        Vector3 cursorPos;
+        Vector3 contentPos = new Vector3(0, 0, 0);
+        Vector3 contextVelocity = Vector3.zero;
+
+        RectTransform contextRect;
+        RectTransform contentRect;
+
+        [HideInInspector] public bool isOn;
+        [HideInInspector] public bool bottomLeft;
+        [HideInInspector] public bool bottomRight;
+        [HideInInspector] public bool topLeft;
+        [HideInInspector] public bool topRight;
+        [HideInInspector] public bool fixedMenu;
+
+        public enum CameraSource { Main, Custom }
+
+        public enum SubMenuBehaviour { Hover, Click }
+
+        void Awake()
+        {
+            if (mainCanvas == null) { mainCanvas = gameObject.GetComponentInParent<Canvas>(); }
+            if (contextAnimator == null) { contextAnimator = gameObject.GetComponent<Animator>(); }
+            if (cameraSource == CameraSource.Main) { targetCamera = Camera.main; }
+
+            contextRect = gameObject.GetComponent<RectTransform>();
+            contentRect = contextContent.GetComponent<RectTransform>();
+            contentPos = new Vector3(vBorderTop, hBorderLeft, 0);
+            gameObject.transform.SetAsLastSibling();
+#if UNITY_2022_1_OR_NEWER
+            subMenuBehaviour = SubMenuBehaviour.Click;
+#endif
+        }
+
+        public void CheckForBounds()
+        {
+            if (uiPos.x <= -100) { contentPos = new Vector3(hBorderLeft, contentPos.y, 0); contentRect.pivot = new Vector2(0f, contentRect.pivot.y); bottomLeft = true; }
+            else { bottomLeft = false; }
+
+            if (uiPos.x >= 100) { contentPos = new Vector3(hBorderRight, contentPos.y, 0); contentRect.pivot = new Vector2(1f, contentRect.pivot.y); bottomRight = true; }
+            else { bottomRight = false; }
+
+            if (uiPos.y <= -75) { contentPos = new Vector3(contentPos.x, vBorderBottom, 0); contentRect.pivot = new Vector2(contentRect.pivot.x, 0f); topLeft = true; }
+            else { topLeft = false; }
+
+            if (uiPos.y >= 75) { contentPos = new Vector3(contentPos.x, vBorderTop, 0); contentRect.pivot = new Vector2(contentRect.pivot.x, 1f); topRight = true; }
+            else { topRight = false; }
+        }
+
+        public void SetContextMenuPosition()
+        {
+#if ENABLE_LEGACY_INPUT_MANAGER
+            cursorPos = Input.mousePosition;
+#elif ENABLE_INPUT_SYSTEM
+            cursorPos = Mouse.current.position.ReadValue();
+#endif
+            uiPos = contextRect.anchoredPosition;
+            CheckForBounds();
+
+            if (mainCanvas.renderMode == RenderMode.ScreenSpaceCamera || mainCanvas.renderMode == RenderMode.WorldSpace)
+            {
+                contextRect.position = targetCamera.ScreenToWorldPoint(cursorPos);
+                contextRect.localPosition = new Vector3(contextRect.localPosition.x, contextRect.localPosition.y, 0);
+                contextContent.transform.localPosition = Vector3.SmoothDamp(contextContent.transform.localPosition, contentPos, ref contextVelocity, 0);
+            }
+
+            else if (mainCanvas.renderMode == RenderMode.ScreenSpaceOverlay)
+            {
+                contextRect.position = cursorPos;
+                contextContent.transform.position = new Vector3(cursorPos.x + contentPos.x, cursorPos.y + contentPos.y, 0);
+            }
+
+            if (fixedMenu == false)
+            {
+                if (trigger != null)
+                {
+                    trigger.SetActive(true);
+                }
+                if (shadow != null)
+                {
+                    shadow.SetActive(true);
+                }
+            }
+        }
+
+        //For fixed context menu
+        public void SetContextMenuPosition(Vector2 worldPos)
+        {
+            uiPos = contextRect.anchoredPosition;
+            CheckForBounds();
+
+            if (mainCanvas.renderMode == RenderMode.ScreenSpaceOverlay)
+            {
+                contentRect.pivot = new Vector2(0f, 1f);
+                contextRect.position = worldPos;
+                contextContent.transform.localPosition = Vector3.zero;
+            }
+
+            if (fixedMenu == true)
+            {
+                if (trigger != null)
+                {
+                    trigger.SetActive(false);
+                }
+                if (shadow != null)
+                {
+                    shadow.SetActive(false);
+                }
+            }
+        }
+
+        public void Open() { contextAnimator.Play("Menu In"); isOn = true; }
+        public void Close() { contextAnimator.Play("Menu Out"); isOn = false; }
+      
+        // Obsolote
+        public void OpenContextMenu() { Open(); }
+        public void CloseOnClick() { Close(); }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (fixedMenu == true)
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ Assets/Modern UI Pack/Scripts/Toggle/CustomToggle.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using ColorUtility = UnityEngine.ColorUtility;
+
+namespace Michsky.MUIP
+{
+    [RequireComponent(typeof(Toggle))]
+    [RequireComponent(typeof(Animator))]
+
+    public class CustomToggle : MonoBehaviour
+    {
+        [HideInInspector] public Toggle toggleObject;
+        [HideInInspector] public Animator toggleAnimator;
+
+        [Header("Settings")]
+        public bool invokeOnAwake;
+ 
+        void Awake()
+        {
+            if (toggleObject == null) 
+            { 
+                toggleObject = gameObject.GetComponent<Toggle>(); 
+            }
+            if (toggleAnimator == null) 
+            { 
+                toggleAnimator = toggleObject.GetComponent<Animator>(); 
+            }
+
+            toggleObject.onValueChanged.AddListener(UpdateStateDynamic);
+            UpdateState();           
+
+            if (invokeOnAwake == true) 
+            { 
+                toggleObject.onValueChanged.Invoke(toggleObject.isOn); 
+            }
+        }
+
+        public void UpdateState()
+        {
+            StopCoroutine("DisableAnimator");
+            toggleAnimator.enabled = true;
+
+            if (toggleObject.isOn) 
+            { 
+                toggleAnimator.Play("On Instant"); 
+            }
+            else
+            {
+                toggleAnimator.Play("Off Instant");
+            }
+            StartCoroutine("DisableAnimator");
+        }
+
+        public void UpdateStateDynamic(bool value)
+        {
+            StopCoroutine("DisableAnimator");
+            toggleAnimator.enabled = true;
+
+            if (toggleObject.isOn) 
+            { 
+                toggleAnimator.Play("Toggle On"); 
+            }
+            else 
+            { 
+                toggleAnimator.Play("Toggle Off"); 
+            }
+
+            StartCoroutine("DisableAnimator");
+        }
+
+        IEnumerator DisableAnimator()
+        {
+            yield return new WaitForSeconds(0.6f);
+            toggleAnimator.enabled = false;
+        }
+
+
+        //Use this method and its overloads for toggle interactable, else wouldn't working visualize disabling.
+        //This is because you cannot subscribe to the standard interactable flag. 
+        public void ToggleInteractable()
+        {
+            ToggleInteractable(!toggleObject.interactable);
+        }
+        
+        public void ToggleInteractable(bool interactable)
+        {
+            toggleObject.interactable = interactable;
+
+            StopCoroutine("DisableAnimator");
+            toggleAnimator.enabled = true;
+
+            if (toggleObject.interactable)
+            {
+                if (toggleObject.isOn)
+                {
+                    toggleAnimator.Play("On Instant");
+                }
+                else
+                {
+                    toggleAnimator.Play("Off Instant");
+                }
+            }
+            else
+            {
+                if (toggleObject.isOn)
+                {
+                    toggleAnimator.Play("Disabled On");
+                }
+                else
+                {
+                    toggleAnimator.Play("Disabled Off");
+                }
+            }
+            StartCoroutine("DisableAnimator");
+        }
+    }
+}
